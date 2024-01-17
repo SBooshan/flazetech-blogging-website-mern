011d9ef778be1eeaab84527f83379010df943d69
SBooshan <156601752+SBooshan@users.noreply.github.com>
2024-01-17 13:27:43 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const path = require("path");
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({credentials:true,origin:'https://flazetech-blogging-website-mern-client.vercel.app/'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
